f45adaaaa50c6a7a7a418a4cd7f57fa221536a21
Diogo Santos <diogo.magalhaescalvo@gmail.com>
2023-03-06 10:53:57 +0000

    First commit

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,59 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    let getMinutes = this.currentTime / 60;
+    return Math.floor(getMinutes);
   }
 
   getSeconds() {
-    // ... your code goes here
+    if (this.currentTime === 0) {
+      return 0;
+    }
+    let sec = this.currentTime % 60;
+    return Math.floor(sec);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let newValue = value.toString();
+    if (newValue.length === 1) {
+      return '0' + value;
+    }
+    return newValue;
   }
+
+  //computeTwoDigitNumber(value){
+  //  return `0${value}`.slice(-2);
+  //}
+  //
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
+    this.currentTime = 0;
     // ... your code goes here
   }
 
   split() {
     // ... your code goes here
+
+    return `${this.computeTwoDigitNumber(
+      this.getMinutes()
+    )}:${this.computeTwoDigitNumber(this.getSeconds())}`;
   }
 }
